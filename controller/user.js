ab096ce6b5d1057e7c9f205f80bc036352ddd52c
Akash4693 <viperholic46@gmail.com>
2025-05-21 15:50:28 +0530

    activation url updated

--- controller/user.js
+++ controller/user.js
@@ -44,7 +44,7 @@ router.post("/create-user", async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+    const activationUrl = `https://trade-x-frontend-omega.vercel.app/activation/${activationToken}`;
 
     try {
       await sendMail({
